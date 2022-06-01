ab6c5b8f9cadd670b5caaa7865a9eecbf703290b
Kailas Thonnithodi <80829462+KailasThonnithodi-2002@users.noreply.github.com>
2022-06-01 19:29:38 +1000

    Delete random.py

--- random.py
+++
@@ -1,8 +0,0 @@
-class Random():
-    def __init__(self):
-        super().__init__()
-
-    # the training action is any random action from within the environment action space
-    def action(self, env):
-        return env.action_space.sample()
-
